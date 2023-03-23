420b4260f239588a4f0347b1094438fdc71eebb7
Azhar.Mehmood <azhar.gondal19@gmail.com>
2023-03-23 14:16:26 +0500

    Pushing the changes to GITHUB

---
+++ app.py
@@ -0,0 +1,33 @@
+from os import name
+import os
+from dotenv import load_dotenv
+from flask import Flask, request
+import psycopg2
+
+CREATE_ROOMS_TABLE = (
+    "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+)
+
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+
+
+load_dotenv()  # loads variables from .env file into environment
+
+app = Flask(__name__)
+url = os.environ.get("DATABASE_URL")  # gets variables from environment
+connection = psycopg2.connect(url)
+
+@app.post("/api/room")
+def create_room():
+    data = request.get_json()
+    name = data["name"]
+    with connection:
+            with connection.cursor() as cursor:
+                 cursor.execute(CREATE_ROOMS_TABLE)
+                 cursor.execute(INSERT_ROOM_RETURN_ID, (name,))
+                 room_id = cursor.fetchone()[0]
+    return {"id": room_id, "message": f"Room {name} created."}, 201
+
+@app.get("/")
+def home():
+    return "Hello world!"
